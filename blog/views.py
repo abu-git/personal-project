cdbe1a8f313ef8c7a472d0cbd63ae836f9740d4e
abu-git <abu.sammie@gmail.com>
2018-11-24 21:56:15 +0200

    blog working but no posts

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+from .models import Post
+
+# Create your views here.
+#class ListView(TemplateView):
+#	template_name = 'blog/blog.html'
+
+#class DetailView(TemplateView):
+#	template_name = 'blog/post.html'
+
--- mySite/settings.py
+++ mySite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'personal.apps.PersonalConfig'
+    'personal.apps.PersonalConfig',
+    'blog'
 ]
 
 MIDDLEWARE = [
